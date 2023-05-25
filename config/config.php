a45b97657c779f8956907668a0a29368eff7db03
hilmyrs123 <hilmyrs123@gmail.com>
2023-05-25 16:14:22 +0700

    2

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+//config untuk memudahkan kalo gunta ganti db
+
+class Config
+{
+    static public $hostname = "localhost";
+    static public $usn = "root";
+    static public $pass = "";
+    static public $db = "vctdb";
+}
